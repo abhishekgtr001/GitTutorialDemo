87a97f199cacf8d5311055e6416c330fbca888f3
Abhishek kumar <abhishekgtr001@gmail.com>
2021-07-07 06:25:22 +0530

    unstaged the file

--- array.java
+++ array.java
@@ -1,12 +1,12 @@
 public class array {
-    public static void main(String args[])
+    public static void main(String args[])//arrays are liner data structures
     {
         int a[] = new int[5];
-        a[0]=2;
+        a[0]=2;//inserting first element in array
         a[1]=4;
         a[2]=8;
         a[3]=4;
-        a[4]=9;
+        a[4]=9;//inserting last element in array
         
         for(int i=0; i<5;i++)
         {
